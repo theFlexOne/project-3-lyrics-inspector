543b51ff31fc480061cbf28b1796f665dfcd8424
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-02-20 00:46:08 -0600

    updating SpotifyController methods for artist and tracks

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -6,15 +6,17 @@ require "rspotify"
 class SpotifyController
   include RSpotify
 
-  def self.get_artist(artist_name)
-    artist = RSpotify::Artist.search(artist_name)
-    binding.pry
+  def self.get_artist_matches(artist_name, limit: 5)
+    RSpotify::Artist.search(artist_name, limit: limit, market: "US")
   end
 
-  def self.get_track(track_name)
-    track = RSpotify::Track.search(track_name)
-    binding.pry
+  def self.get_track_matches(track_name, limit: 5)
+    RSpotify::Track.search(track_name, limit: limit, market: "US")
   end
 
   private
+
+  def build_artist_hash_for_db
+    SpotifyController.get_artist
+  end
 end
