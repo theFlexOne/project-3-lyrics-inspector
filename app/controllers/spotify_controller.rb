27c1282205070cb6d11f1fa129afc0379845bf4f
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-02-19 23:58:32 -0600

    moved RSpotify authentication into config/environment.rb

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,18 +1,20 @@
 require "rspotify"
 
-AUTH_IDS = YAML.load_file("keys.yml")
-RSpotify.authenticate AUTH_IDS[:spotify_client_id], AUTH_IDS[:spotify_client_secret]
+# AUTH_IDS = YAML.load_file("keys.yml")
+# RSpotify.authenticate AUTH_IDS[:spotify_client_id], AUTH_IDS[:spotify_client_secret]
 
 class SpotifyController
   include RSpotify
 
   def self.get_artist(artist_name)
-    artist = RSpotify::Artist.search(artist_name).first
+    artist = RSpotify::Artist.search(artist_name)
     binding.pry
   end
 
   def self.get_track(track_name)
-    track = RSpotify::Track.search(track_name).first
+    track = RSpotify::Track.search(track_name)
     binding.pry
   end
+
+  private
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,16 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
-ENV['RACK_ENV'] ||= "development"
+ENV["RACK_ENV"] ||= "development"
 
 # Require in Gems
-require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+require "bundler/setup"
+Bundler.require(:default, ENV["RACK_ENV"])
 
 # Require in all files in 'app' directory
-require_all 'app'
+require_all "app"
+
+# Authenticate SpotifyController
+keys = YAML.load_file("keys.yml")
+id = keys[:spotify_client_id]
+secret = keys[:spotify_client_secret]
+RSpotify::authenticate(id, secret)
