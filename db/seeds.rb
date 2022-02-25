2b1a92649550ce37276f8816e86e0d5a842b8e73
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-02-25 01:23:44 -0600

    updated & fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,12 @@ playlists.each do |playlist|
   Playlist.create(name: playlist.name, spotify_id: playlist.id)
 end
 
+# preloading genres...
+top_50_genres = RSpotify::Category.list(country: "US", limit: 50).map { |g| { name: g.name, spotify_id: g.id } } # <- max is 50
+top_50_genres.each do |genre|
+  Genre.find_or_create_by(name: genre[:name], spotify_id: genre[:id])
+end
+
 Playlist.all.each do |playlist|
   RSpotify::Playlist.find("3174uilmw5y5bls52mcyadu6mpwe", playlist.spotify_id).tracks.map do |track|
     s_artist = track.artists.first
@@ -16,16 +22,15 @@ Playlist.all.each do |playlist|
     artist = Artist.find_or_create_by(name: s_artist.name, spotify_id: s_artist.id, popularity: s_artist.popularity)
     album = Album.find_or_create_by(name: s_album.name, spotify_id: s_album.id, release_date: s_album.release_date, artist_id: artist.id)
 
-    s_album.genres.each do |s_genre|
-      genre = Genre.find_or_create_by(name: s_genre.name, spotify_id: s_genre.id)
-      AlbumGenre.find_or_create_by(genre_id: genre.id, album_id: album.id)
+    s_artist.genres.each do |genre_name|
+      genre_name_spaces = genre_name.titlecase
+      genre = Genre.find_or_create_by(name: genre_name_spaces)
+      ArtistGenre.find_or_create_by(genre_id: genre.id, artist_id: artist.id)
     end
 
-    new_track = playlist.tracks.find_or_create_by(name: track.name, spotify_id: track.id, duration_s: (track.duration_ms / 1000))
+    new_track = playlist.tracks.find_or_create_by(name: track.name, spotify_id: track.id, duration_s: (track.duration_ms / 1000), album_id: album.id)
     PlaylistTrack.create(playlist_id: playlist.id, track_id: new_track.id)
   end
 end
-
-binding.pry
 
 puts "Seeding done!"
