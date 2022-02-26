ff11e2257929d967391079967a50aff4603798fe
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-02-26 09:18:56 -0600

    filled out playlist.rb

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,7 @@
 class Playlist < ActiveRecord::Base
   has_many :playlist_tracks
   has_many :tracks, through: :playlist_tracks
+  has_many :albums, through: :tracks
+  has_many :artists, through: :albums
+  has_many :genres, through: :artists
 end
