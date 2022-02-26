18bb1690c3d6390ab639d21f67ab36d8f0914b9b
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-02-26 10:56:49 -0600

    set up relationship between playlists & users

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,4 +4,10 @@ class Playlist < ActiveRecord::Base
   has_many :albums, through: :tracks
   has_many :artists, through: :albums
   has_many :genres, through: :artists
+  belongs_to :user
+
+  # def self.by_user(id)
+  #   RSpotify::User.find(id)
+  # end
+
 end
