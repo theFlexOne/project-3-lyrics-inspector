82f7c037df6af157f856206c54975a961c5df89a
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-02-26 10:57:13 -0600

    added build_user_params method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_many :albums, through: :tracks
   has_many :artists, through: :albums
   has_many :genres, through: :artists
+
+  def self.build_user_params(id = ENV["MY_SPOTIFY_USER_ID"])
+    user = RSpotify::User.find(id)
+    {
+      username: user.display_name,
+      spotify_id: user.id,
+      spotify_url: user.external_urls["spotify"],
+    }
+  end
 end
