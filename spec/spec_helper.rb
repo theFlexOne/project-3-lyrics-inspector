3cec5dbf2ee67b6fdd478c2f01fbbd9197cd4a34
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-02-26 15:05:56 -0600

    configured rspec for project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,8 +44,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 =begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
@@ -97,4 +97,9 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
 end
+
+require "bundler"
+Bundler.require(:default, :test)
+require File.expand_path("../../config/environment.rb", __FILE__)
